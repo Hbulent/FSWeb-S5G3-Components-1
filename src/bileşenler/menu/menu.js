aab068df5951cdd63034ad6158cf274bce70db60
Hbulent <bulent5354@gmail.com>
2023-04-03 18:40:00 +0300

    proje tamamlandı

--- src/bileşenler/menu/menu.js
+++ src/bileşenler/menu/menu.js
@@ -1,36 +1,48 @@
-import './Menu.less'
+import "./Menu.less";
 
 // Kullanacağımız veri bu, üzerinde çalışın ama henüz hiçbir şeyi değiştirmeyin.
 let menuElemanlari = [
-  'Gündem',
-  'Dünya',
+  "Gündem",
+  "Dünya",
   "Ekonomi",
-  'Yazarlar',
-  'Burç Yorumları',
-  'Diğer'
+  "Yazarlar",
+  "Burç Yorumları",
+  "Diğer",
 ];
 
 /*
   Adım 1: Aşağıdaki etiketlee gibi bir menü oluşturmak için 'menuYapici' adlı bir bileşen yazın:
-
   <div class="menu">
     <ul>
       {tüm menü elemanları <li> içlerine}
     </ul>
   </div>
-
   'menuYapici' fonksiyonu tek argümanı olarak bir menü elemanları dizisini alır (birinci parametre).
-
   Adım 2: Fonksiyonun içinde, dizideki her öğe için bir liste <li> öğesi oluşturarak dizi üzerinde yineleme yapın.
   Tüm bu öğeleri <ul>'a ekleyin
-
   Adım 3: Hala fonksiyon içindeyiz, DOM'dan menü düğmesini seçin ('menu-button' sınıfına(class) sahip öğe).
-
   Adım 4: Menü butonuna bir `click` event dinleyicisi ekleyin. Butona her tıklanıldığında div.menu classına `menu--open`('menu' class'ına sahip olan div) ekleyip/silecek(toogle).
   
   İPUCU: Javascript'te toogle metotları bir öğeyi bir elemana ekleyip/çıkarmaya yarar. Örnek olarak bir öğeye tıklandığında bir class toogle'ı kullanıldığında, o öğreye tıklandığında eğer o class ismi öğede yoksa ekler, eğer o class ismi öğede varsa siler. Toogle - aç kapa manasına da gelir.
-
   Adım 5: Oluştrulan div.menu 'yü döndürmeyi unutmayın.
-
   Adım 6: 'menuYapici' fonksiyonunu ve 'menuElemanlari' dizisini kullanarak menüyü oluşturun, ve döndürülen menüyü header'e ekleyin.
 */
+function menuYapici(menuElemanlari) {
+  const menuOlustur = document.createElement("div");
+  menuOlustur.classList.add("menu");
+  const ulOlustur = document.createElement("ul");
+  const clickKullan = document.querySelector(".menu-button");
+  clickKullan.addEventListener("click", () => {
+    menuOlustur.classList.toggle("menu--open");
+  });
+  for (let i = 0; i < menuElemanlari.length; i++) {
+    const liOlustur = document.createElement("li");
+    liOlustur.textContent = menuElemanlari[i];
+    ulOlustur.appendChild(liOlustur);
+  }
+  menuOlustur.appendChild(ulOlustur);
+  const headerKismi = document.querySelector(".header");
+  headerKismi.appendChild(menuOlustur);
+  return menuOlustur;
+}
+menuYapici(menuElemanlari);
